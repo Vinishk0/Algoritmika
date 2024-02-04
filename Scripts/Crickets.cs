ada79deb57a87fae32b4c0bf8ce835932401bc31
Vinishk0 <94686362+Vinishk0@users.noreply.github.com>
2024-02-04 17:26:07 +0500

    My first commit

---
+++ Scripts/Crickets.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    private void Update()
+    {
+        // Каждый кадр выводит "сrickets", можно было сделать через Debug.Log()
+        print("crickets");
+    }
+}
